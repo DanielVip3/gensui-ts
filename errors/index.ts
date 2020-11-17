652eff54953f39911cac9fd0d89604f503db5ff0
DanielVip3 <danieledemartino.72004@gmail.com>
2020-11-17 15:17:19 +0100

    Added more exports from the single errors file
    Bots and events errors were not exported from that file

--- errors/index.ts
+++ errors/index.ts
@@ -1,4 +1,7 @@
 export * from './commands';
+export * from './events';
 export * from './exception-handler';
 export * from './filters';
+export * from './consumers';
 export * from './interceptors';
+export * from './bot';
