eecabebd04af6cf9fa069982e1c13c860d487321
DanielVip3 <danieledemartino.72004@gmail.com>
2020-11-17 22:07:01 +0100

    Added payload parameter to InlineFilter for events

--- filters/events/InlineFilter.ts
+++ filters/events/InlineFilter.ts
@@ -2,8 +2,8 @@ import { EventFilter } from '../../classes/events/EventFilter';
 import { EventPayload, EventContext } from '../../classes/events/Event';
 import { ClientEvents } from 'discord.js';
 
-export interface InlineFilterCallback {
-    (ctx: EventContext): boolean|Promise<boolean>,
+export interface InlineFilterCallback<K extends keyof ClientEvents> {
+    (payload: EventPayload<K>, ctx: EventContext): boolean|Promise<boolean>,
 };
 
 /*
@@ -11,18 +11,18 @@ A simple filter which allows an inline callback function to determine if to exec
 DOES NOT pair with exceptions - i.e. you can't catch exceptions called inside this filter (for now).
 */
 export default class InlineFilter<K extends keyof ClientEvents> implements EventFilter<K> {
-    private readonly callback: InlineFilterCallback;
+    private readonly callback: InlineFilterCallback<K>;
     public readonly whitelist: boolean;
     
-    constructor(callback: InlineFilterCallback, whitelist: boolean = true) {
+    constructor(callback: InlineFilterCallback<K>, whitelist: boolean = true) {
         this.callback = callback;
         this.whitelist = whitelist;
     }
 
     public async filter(payload: EventPayload<K>, ctx: EventContext): Promise<boolean> {
         return this.whitelist 
-                            ? !!await this.callback(ctx)
-                            : !await this.callback(ctx);
+                            ? !!await this.callback(payload, ctx)
+                            : !await this.callback(payload, ctx);
     }
 
     public handleError(filtered: boolean): void { return; }
