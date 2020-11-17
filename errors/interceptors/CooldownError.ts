9ef5aa7a2c53ff4549f6831acef166d791177192
DanielVip3 <danieledemartino.72004@gmail.com>
2020-11-17 15:16:18 +0100

    Fixed the wrong name 'CommandCooldownError' to the actual name 'CooldownError'

--- errors/interceptors/CooldownError.ts
+++ errors/interceptors/CooldownError.ts
@@ -1,13 +1,13 @@
 import GenericInterceptorError from './GenericInterceptorError';
 
 /* Called when user is in cooldown */
-export default class CommandCooldownError extends GenericInterceptorError {
+export default class CooldownError extends GenericInterceptorError {
     remaining: number;
 
     constructor(m: string, started?: Date, cooldownTime?: number) {
         super(m);
         
-        this.name = "CommandCooldownError";
+        this.name = "CooldownError";
         if (started && cooldownTime) {
             this.remaining = new Date().getTime() - new Date(started.getTime() + cooldownTime).getTime();
         }
