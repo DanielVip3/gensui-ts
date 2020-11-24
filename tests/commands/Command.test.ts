814d50e3f9cde235e1b56c15675372a3ea31c2d5
DanielVip3 <danieledemartino.72004@gmail.com>
2020-11-24 22:17:24 +0100

    Added more command consumers tests and did some refactoring

--- tests/commands/Command.test.ts
+++ tests/commands/Command.test.ts
@@ -3,6 +3,9 @@ import { CommandCallOptions } from '../../classes/commands/CommandCallOptions';
 import { Filters, InlineCommandFilter } from '../../filters/Filters';
 import { Interceptors, InlineCommandInterceptor } from '../../interceptors/Interceptors';
 import { Consumers, InlineCommandConsumer } from '../../consumers/Consumers';
+import { CommandExceptionHandler } from '../../classes/exception-handler/CommandExceptionHandler';
+import { CommandInterceptorResponse } from '../../classes/commands/CommandInterceptor';
+import { CommandConsumerResponse } from '../../classes/commands/CommandConsumer';
 
 import Bot from '../../classes/Bot';
 
@@ -15,8 +18,6 @@ chai.use(chaiAsPromised);
 import { Client, Guild, Message, SnowflakeUtil, TextChannel } from 'discord.js';
 
 import * as sinon from 'sinon';
-import { CommandExceptionHandler } from '../../classes/exception-handler/CommandExceptionHandler';
-import { CommandInterceptorResponse } from '../../classes/commands/CommandInterceptor';
 
 const botMockID = new Bot({ // a bot mock to test repeating ids error
     name: "mock",
@@ -328,6 +329,81 @@ describe("Command", function() {
                     names: "test",
                     handler: sinon.fake(),
                 })).to.have.property("consumers").and.to.have.members([consumer]);
+            });
+
+            it("calls consumers and flow goes forward if no consumer was passed", async function() {
+                const command: Command = new Command({
+                    id: "test",
+                    names: "test",
+                    handler: sinon.fake(),
+                });
+
+                const response = await command.callConsumers(commandContextMock(command), "return data by command test");
+
+                expect(response).to.be.ok;
+                expect(response.next).to.be.true;
+                expect(response.data).to.be.empty;
+            });
+
+            it("calls consumers, in order, with correct parameters", async function() {
+                const callback1 = sinon.stub().returns({ next: true });
+                const callback2 = sinon.stub().returns({ next: true });
+
+                const consumer1: InlineCommandConsumer = Consumers.Commands.Inline(callback1);
+                const consumer2: InlineCommandConsumer = Consumers.Commands.Inline(callback2);
+
+                const command: Command = new Command({
+                    id: "test",
+                    names: "test",
+                    consumers: [consumer1, consumer2],
+                    handler: sinon.fake(),
+                });
+
+                await command.callConsumers(commandContextMock(command), "return data by command test");
+
+                sinon.assert.calledWith(callback1, commandContextMock(command), "return data by command test");
+                sinon.assert.calledWith(callback2, commandContextMock(command), "return data by command test");
+
+                expect(callback2.calledImmediatelyAfter(callback1)).to.be.true;
+            });
+
+            it("stops consumers' flow if one returns next as false", async function() {
+                const callback1 = sinon.stub().returns({ next: false });
+                const callback2 = sinon.stub().returns({ next: true });
+
+                const consumer1: InlineCommandConsumer = Consumers.Commands.Inline(callback1);
+                const consumer2: InlineCommandConsumer = Consumers.Commands.Inline(callback2);
+
+                const command: Command = new Command({
+                    id: "test",
+                    names: "test",
+                    consumers: [consumer1, consumer2],
+                    handler: sinon.fake(),
+                });
+
+                await command.callConsumers(commandContextMock(command), "return data by command test");
+
+                sinon.assert.calledWith(callback1, commandContextMock(command), "return data by command test");
+                sinon.assert.notCalled(callback2);
+            });
+
+            it("merges consumers' returned data", async function() {
+                const callback1 = sinon.stub().returns({ next: true, data: { foo: 1, bar: 1 } });
+                const callback2 = sinon.stub().returns({ next: true, data: { foo: 2 } });
+
+                const consumer1: InlineCommandConsumer = Consumers.Commands.Inline(callback1);
+                const consumer2: InlineCommandConsumer = Consumers.Commands.Inline(callback2);
+
+                const command: Command = new Command({
+                    id: "test",
+                    names: "test",
+                    consumers: [consumer1, consumer2],
+                    handler: sinon.fake(),
+                });
+
+                const response: CommandConsumerResponse = await command.callConsumers(commandContextMock(command), "return data by command test");
+
+                expect(response.data).to.be.ok.and.to.include({ foo: 2, bar: 1 });
             });
         });
 
