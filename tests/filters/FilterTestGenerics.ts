94e2bca38de60c95ce1afafcaa639aa2fdbfbc5d
DanielVip3 <danieledemartino.72004@gmail.com>
2020-11-26 23:56:00 +0100

    Added error thrown check for filters' generic tests

--- tests/filters/FilterTestGenerics.ts
+++ tests/filters/FilterTestGenerics.ts
@@ -1,11 +1,17 @@
+import * as chai from 'chai';
 import { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
 
 /*
-Being a filter a class which implements a generic abstract class which exists only in TypeScript, we can't test at runtime that a filter class implements the abstract class.
-So, we'll use a simple duck checking system: if it looks like a filter, has a filter method like a filter, and has an handleError method like a filter, then probably is a filter.
-"parameters" SHOULD NOT specify the "whitelist" parameter (the last one) because it gets specified and tested automatically in this function.
+* Being a filter a class which implements a generic abstract class which exists only in TypeScript, we can't test at runtime that a filter class implements the abstract class.
+* So, we'll use a simple duck checking system: if it looks like a filter, has a filter method like a filter, and has an handleError method like a filter, then probably is a filter.
+*
+* This function expects "parameters" to be the actual parameters which would return true, when combined with "passedFilterOptions". If they're wrong and they don't return true, tests do fail.
+* "parameters" SHOULD NOT specify the "whitelist" parameter (the last one) because it gets specified and tested automatically in this function.
 */
-export default function shouldBeAFilter(filter: any, parameters: any[], passedFilterOptions: any[]) {
+export default function shouldBeAFilter(filter: any, parameters: any[], passedFilterOptions: any[], shouldThrowErrorIfFalse: boolean = false, thrownError: any = null) {
     it("instantiates", function() {
         expect(new filter(...parameters)).to.be.instanceOf(filter);
     });
@@ -31,4 +37,12 @@ export default function shouldBeAFilter(filter: any, parameters: any[], passedFi
         const trueWhitelistCall = await (new filter(...parameters, true)).filter(...passedFilterOptions);
         expect(falseWhitelistCall).to.be.equal(!trueWhitelistCall);
     });
+
+    if (shouldThrowErrorIfFalse && thrownError) {
+        it("throws error from handleError when returned result is false", function() {
+            const filterH = new filter(...parameters, true);
+            const result = filterH.filter(...passedFilterOptions);
+            expect(() => filterH.handleError(!result, ...parameters)).to.throw(thrownError);
+        });
+    }
 };
