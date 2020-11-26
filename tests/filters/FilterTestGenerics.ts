0a7b348b963f5feb2eb8e0020d978b342ea76681
DanielVip3 <danieledemartino.72004@gmail.com>
2020-11-26 19:31:15 +0100

    Specified in a comment that whitelist parameter shouldn't be put inside the parameters array in the generic test function because it's automatically put and tested by the function itself

--- tests/filters/FilterTestGenerics.ts
+++ tests/filters/FilterTestGenerics.ts
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 /*
 Being a filter a class which implements a generic abstract class which exists only in TypeScript, we can't test at runtime that a filter class implements the abstract class.
 So, we'll use a simple duck checking system: if it looks like a filter, has a filter method like a filter, and has an handleError method like a filter, then probably is a filter.
+"parameters" SHOULD NOT specify the "whitelist" parameter (the last one) because it gets specified and tested automatically in this function.
 */
 export default function shouldBeAFilter(filter: any, parameters: any[], passedFilterOptions: any[]) {
     it("instantiates", function() {
