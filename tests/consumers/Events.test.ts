66ec83f12c44841d46ac9b050c8ff249cc95f1f8
DanielVip3 <danieledemartino.72004@gmail.com>
2020-11-30 11:20:19 +0100

    Added events consumers tests, specifically for InlineEventConsumer

---
+++ tests/consumers/Events.test.ts
@@ -0,0 +1,43 @@
+import { InlineEventConsumer } from '../../consumers/Consumers';
+import { Event, EventContext, EventPayload } from '../../classes/events/Event';
+import { EventConsumerResponse } from '../../classes/events/EventConsumer';
+import { Client, Guild, Message, SnowflakeUtil, TextChannel } from 'discord.js';
+
+import * as chai from 'chai';
+import { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+
+import * as sinon from 'sinon';
+
+import shouldBeAConsumer from './ConsumerTestGenerics';
+
+const discordClientMock = new Client();
+const messageMock = new Message(discordClientMock, { id: SnowflakeUtil.generate() }, new TextChannel(new Guild(discordClientMock, { id: SnowflakeUtil.generate() }), { id: SnowflakeUtil.generate() }));
+const eventMock: Event = new Event({
+    id: "test",
+    type: "message",
+    handler: sinon.fake(),
+});
+
+const eventPayloadMock = [messageMock] as EventPayload<"message">;
+const eventContextMock = { event: eventMock } as EventContext;
+const eventReturnDataMock = "return data by event test";
+
+const consumerResponseMock = { next: true } as EventConsumerResponse;
+
+describe("Events built-in consumers", function() {
+    describe("InlineConsumer", function() {
+        shouldBeAConsumer(InlineEventConsumer, [sinon.stub().returns(consumerResponseMock)], [eventPayloadMock, eventContextMock, eventReturnDataMock]);
+
+        it("accepts and calls a callback with correct parameters", async function() {
+            const callback = sinon.stub().returns(consumerResponseMock);
+            const consumer: InlineEventConsumer<"message"> = new InlineEventConsumer(callback);
+
+            await consumer.consume(eventPayloadMock, eventContextMock, eventReturnDataMock);
+
+            sinon.assert.calledWith(callback, eventPayloadMock, eventContextMock, eventReturnDataMock);
+        });
+    });
+});
