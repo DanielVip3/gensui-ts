800559e406c2f571541d373f9ef53755f8d4a0dd
DanielVip3 <danieledemartino.72004@gmail.com>
2020-11-30 12:54:44 +0100

    Added an CommandSideEffect mock to test side-effect class

---
+++ tests/mocks/CommandSideEffect.mock.ts
@@ -0,0 +1,18 @@
+import { CommandSideEffect } from "../../classes/commands/CommandConsumer";
+
+export default class CommandSideEffectMock extends CommandSideEffect {
+    public stub: Function;
+    public callStub: boolean;
+
+    constructor(stub, callStub) {
+        super();
+
+        this.stub = stub;
+        this.callStub = callStub;
+    }
+
+    public effect(context) {
+        if (this.stub) this.stub(context);
+        context.testDone = true;
+    }
+};
