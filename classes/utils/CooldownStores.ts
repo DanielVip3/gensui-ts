bef0020d90a59a0069007e51be524d8fe4bfb2c6
DanielVip3 <danieledemartino.72004@gmail.com>
2020-12-04 01:02:09 +0100

    Minor but sparse refactoring, more in description
    First of all, this refactoring was made to reach 100% code coverage, so I added some ignore comments for unreachable lines. Also, I changed possible return values for getCooldown function for CooldownStores to have an undefined return value: in fact, the undefined value should be used when no item is found, and the null value when it wasn't possible to get the cooldown because of other kind of reasons (like when the userId is not passed). I also added an error for when cooldownIdentifierKey is not passed for a RedisCooldownStore and an error for when options object in general is not passed to a RedisCooldownStore. Last, but not least, I simplified istanbul's job by rewriting some weird if statements.

--- classes/utils/CooldownStores.ts
+++ classes/utils/CooldownStores.ts
@@ -26,7 +26,7 @@ export abstract class CooldownStore {
 
     constructor(options?: CooldownStoreOptions) {}
 
-    abstract getCooldown(userId?: string): CooldownStoreObject|null|Promise<CooldownStoreObject|null>;
+    abstract getCooldown(userId?: string): CooldownStoreObject|null|undefined|Promise<CooldownStoreObject|null>|Promise<CooldownStoreObject|null|undefined>;
     abstract isInCooldown(userId?: string): boolean|Promise<boolean>;
     abstract increaseCooldown(userId?: string): void|Promise<void>;
     abstract deleteCooldown(userId?: string): boolean|Promise<boolean>;
@@ -37,23 +37,21 @@ export class MemoryCooldownStore extends CooldownStore {
     public readonly cooldownTime: number = 60 * 60 * 1000;
     protected maxTimes: number = 1;
     
-    constructor(options?: CooldownStoreOptions) {
+    constructor(options: CooldownStoreOptions = {}) {
         super(options);
 
         this.store = {};
 
-        if (options) {
-            if (options.cooldownTime) this.cooldownTime = options.cooldownTime;
-            if (options.maxTimes) this.maxTimes = options.maxTimes;
-        }
+        if (options.cooldownTime) this.cooldownTime = options.cooldownTime;
+        if (options.maxTimes) this.maxTimes = options.maxTimes;
     }
 
-    getCooldown(userId?: string): CooldownStoreObject|null {
+    getCooldown(userId: string): CooldownStoreObject|null {
         if (!userId) return null;
         else return this.store[userId] as CooldownStoreObject;
     }
 
-    isInCooldown(userId?: string): boolean {
+    isInCooldown(userId: string): boolean {
         if (!userId) return false;
 
         if (this.store[userId]) {
@@ -67,8 +65,9 @@ export class MemoryCooldownStore extends CooldownStore {
         } else return false;
     }
 
-    increaseCooldown(userId?: string, called?: Date): void {
-        if (!userId) return undefined;
+    increaseCooldown(userId: string, called?: Date): void {
+        /* istanbul ignore if */
+        if (!userId) return;
         if (!called) called = new Date();
 
         if (!this.store[userId]) this.store[userId] = ({ called, times: 1 } as CooldownStoreObject);
@@ -82,7 +81,7 @@ export class MemoryCooldownStore extends CooldownStore {
         }
     }
 
-    deleteCooldown(userId?: string): boolean {
+    deleteCooldown(userId: string): boolean {
         if (!userId) return false;
 
         if (this.store[userId]) {
@@ -108,6 +107,8 @@ export class RedisCooldownStore extends CooldownStore {
     constructor(options: RedisCooldownStoreOptions) {
         super(options);
 
+        if (!options) throw "RedisCooldownStore needs an options object with configuration parameters.";
+
         if (!options.store || (options.store instanceof IORedis === false && options.store instanceof TestIORedis === false)) throw "RedisCooldownStore needs an IORedis Client in the options field `store`.";
         else this.store = options.store;
 
@@ -115,36 +116,34 @@ export class RedisCooldownStore extends CooldownStore {
         if (options.cooldownHashKey) this.cooldownHashKey = options.cooldownHashKey;
         if (options.maxTimes) this.maxTimes = options.maxTimes;
 
-        if (options && options.cooldownIdentifierKey) {
+        if (options.cooldownIdentifierKey) {
             this.cooldownHashKey += `.${options.cooldownIdentifierKey}`;
-        } else if (!options || !options.cooldownIdentifierKey) {
-            this.cooldownHashKey += `.${randomstring.generate(8)}`;
-        }
+        } else throw "RedisCooldownStore needs a `cooldownIdentifierKey` field to specify a key which identifies the cooldown's command in the redis store.";
     }
 
-    protected composeUserKey(userId?: string): string {
+    protected composeUserKey(userId: string): string {
         return `${this.cooldownHashKey}.${userId}`;
     }
 
-    async getCooldown(userId?: string): Promise<CooldownStoreObject|null> {
+    async getCooldown(userId: string): Promise<CooldownStoreObject|undefined|null> {
         if (!userId) return null;
 
         const cooldownUserString: string|null = await this.store.get(this.composeUserKey(userId));
-        let cooldownUserObject: CooldownStoreObject|null = null;
-        if (cooldownUserString) cooldownUserObject = JSON.parse(cooldownUserString) as CooldownStoreObject || null;
+        let cooldownUserObject: CooldownStoreObject|undefined = undefined;
+        if (cooldownUserString) cooldownUserObject = JSON.parse(cooldownUserString) as CooldownStoreObject /* istanbul ignore next */ || undefined;
 
         return cooldownUserObject;
     }
 
-    async isInCooldown(userId?: string): Promise<boolean> {
+    async isInCooldown(userId: string): Promise<boolean> {
         if (!userId) return false;
 
-        const cooldownUserObject: CooldownStoreObject|null = await this.getCooldown(userId);
+        const cooldownUserObject: CooldownStoreObject|null|undefined = await this.getCooldown(userId);
 
         if (cooldownUserObject) {
             if (cooldownUserObject.times >= this.maxTimes) {
                 if (new Date(new Date(cooldownUserObject.called).getTime() + this.cooldownTime) <= new Date()) {
-                    /* istanbul ignore else impossible to reach except in cases where the cooldown delete fails (and it can't happen except if you volountarily edit the store property, which shouldn't be done) */
+                    /* istanbul ignore else: impossible to reach except in cases where the cooldown delete fails (and it can't happen except if you volountarily edit the store property, which shouldn't be done) */
                     if (this.deleteCooldown(userId)) return false;
                     else return true;
                 } else return true;
@@ -152,11 +151,12 @@ export class RedisCooldownStore extends CooldownStore {
         } else return false;
     }
 
-    async increaseCooldown(userId?: string, called?: Date): Promise<void> {
-        if (!userId) return undefined;
+    async increaseCooldown(userId: string, called?: Date): Promise<void> {
+        /* istanbul ignore if: it doesn't make sense to test because this method doesn't return nothing in any case so you can't know if this if returned or not without doing anything */
+        if (!userId) return;
         if (!called) called = new Date();
 
-        const cooldownUserObject: CooldownStoreObject|null = await this.getCooldown(userId);
+        const cooldownUserObject: CooldownStoreObject|null|undefined = await this.getCooldown(userId);
 
         if (!cooldownUserObject) {
             this.store.set(this.composeUserKey(userId), JSON.stringify({ called, times: 1 } as CooldownStoreObject));
@@ -172,7 +172,7 @@ export class RedisCooldownStore extends CooldownStore {
         }
     }
 
-    async deleteCooldown(userId?: string): Promise<boolean> {
+    async deleteCooldown(userId: string): Promise<boolean> {
         if (!userId) return false;
 
         return !!await this.store.unlink(this.composeUserKey(userId));
