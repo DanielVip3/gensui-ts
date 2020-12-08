5aeecd8d9f6c821752f3adfb87bebfa31a7050de
DanielVip3 <danieledemartino.72004@gmail.com>
2020-12-08 18:37:16 +0100

    Added exceptionShouldBeHandled function test file and describe call

---
+++ tests/utils/exceptionShouldBeHandled.test.ts
@@ -0,0 +1,7 @@
+import exceptionShouldBeHandled from "../../classes/utils/exceptionShouldBeHandled";
+
+/* The first parameter to this method is the thrown exception, and the second is the accepted exception */
+
+describe("exceptionShouldBeHandled function", function() {
+    
+});
