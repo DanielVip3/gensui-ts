942e08aebfe894976e2dbe22b3e24c17565c7616
DanielVip3 <danieledemartino.72004@gmail.com>
2020-12-08 18:40:44 +0100

    Added tests which should be true when it is the same object/instance for accepted errors and thrown exception

--- tests/utils/exceptionShouldBeHandled.test.ts
+++ tests/utils/exceptionShouldBeHandled.test.ts
@@ -1,7 +1,23 @@
 import exceptionShouldBeHandled from "../../classes/utils/exceptionShouldBeHandled";
 
+import { expect } from "chai";
+
 /* The first parameter to this method is the thrown exception, and the second is the accepted exception */
 
 describe("exceptionShouldBeHandled function", function() {
-    
+    it("returns true if passed error and accepted error are an instance of the same object", function() {
+        expect(exceptionShouldBeHandled(new SyntaxError(), new SyntaxError())).to.be.true;
+    });
+
+    it("returns true if passed error and accepted error are the constructor of the same class", function() {
+        expect(exceptionShouldBeHandled(SyntaxError, SyntaxError)).to.be.true;
+    });
+
+    it("returns true if passed error is an instance and accepted error is the constructor, but both of the same class", function() {
+        expect(exceptionShouldBeHandled(new SyntaxError(), SyntaxError)).to.be.true;
+    });
+
+    it("returns true if passed error is the constructor and accepted error is an instance, but both of the same class", function() {
+        expect(exceptionShouldBeHandled(SyntaxError, new SyntaxError())).to.be.true;
+    });
 });
