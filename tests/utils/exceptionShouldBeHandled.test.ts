e08acb511599bbd1bcd3f465f5d7e006d17d7d32
DanielVip3 <danieledemartino.72004@gmail.com>
2020-12-08 18:43:22 +0100

    Added tests which should be false when it's not the same object/instance for accepted errors and thrown exception

--- tests/utils/exceptionShouldBeHandled.test.ts
+++ tests/utils/exceptionShouldBeHandled.test.ts
@@ -20,4 +20,20 @@ describe("exceptionShouldBeHandled function", function() {
     it("returns true if passed error is the constructor and accepted error is an instance, but both of the same class", function() {
         expect(exceptionShouldBeHandled(SyntaxError, new SyntaxError())).to.be.true;
     });
+
+    it("returns false if passed error and accepted error are not an instance of the same object", function() {
+        expect(exceptionShouldBeHandled(new SyntaxError(), new TypeError())).to.be.false;
+    });
+
+    it("returns false if passed error and accepted error are not the constructor of the same class", function() {
+        expect(exceptionShouldBeHandled(SyntaxError, TypeError)).to.be.false;
+    });
+
+    it("returns false if passed error is an instance and accepted error is the constructor, but not of the same class", function() {
+        expect(exceptionShouldBeHandled(new SyntaxError(), TypeError)).to.be.false;
+    });
+
+    it("returns false if passed error is the constructor and accepted error is an instance, but not both of the same class", function() {
+        expect(exceptionShouldBeHandled(SyntaxError, new TypeError())).to.be.false;
+    });
 });
